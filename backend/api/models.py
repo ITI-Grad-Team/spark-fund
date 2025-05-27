c9a18f841f13e1b7882ec48e4be21594180ea9a3
Ahmed Hani <ahmedhani9007@gmail.com>
2025-05-27 23:11:51 +0300

    create project model

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=11, blank=True, null=True, unique=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+class Project():
+    pass
