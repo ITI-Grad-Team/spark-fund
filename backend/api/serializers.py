060ad03ea1e2a1b03ee1c19c4b20a2c68c668854
Ahmed Hani <ahmedhani9007@gmail.com>
2025-05-28 08:10:30 +0300

    Add serializers for Tag, Category, ProjectImage

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,8 @@
 import re
 from rest_framework import serializers
 from api.models import CustomUser
+from .models import Project, Tag, Category, ProjectImage
+from django.contrib.auth import get_user_model
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +15,23 @@ class CustomUserSerializer(serializers.ModelSerializer):
             if value and not re.match(r'^01[0-2,5]{1}[0-9]{8}$', value):
                 raise serializers.ValidationError("Must be a valid Egyptian Phone Number")
             return value    
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+
+class CategorySerializer(serializers.ModelSerializer):
+     class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectImage
+        fields = ['id', 'name']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    pass
