d7e284f234ae15e4f06b15cb1968bef0c81ff5e1
Galaluddin Owais <galaluddinowais@gmail.com>
2025-05-30 01:05:29 +0200

    setup react packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
 import ProjectDetails from "./pages/ProjectDetails/ProjectDetails";
 import CreateProject from "./pages/CreateProject/CreateProject";
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
