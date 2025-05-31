afcd0a5d4674ec56f0a8a21a335660c6fcdb87b2
Othman Ahmed <othmanahmed258@gmail.com>
2025-05-31 22:37:41 +0300

    fix the edit icon background

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,21 @@
+import "./Home.css";
+
 const Home = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <section className="container-fluid">
+      <section className="container">
+        <h1>Make an impact to the world.</h1>
+
+        <p>We’re offer complete solution to launch your social movements.</p>
+
+        <div>
+          <button>Get Started</button>
+
+          <button>Learn More</button>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default Home;
