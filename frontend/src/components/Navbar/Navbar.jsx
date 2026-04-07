c939cefbb4b8623918b34f5052d38e9f36e38580
Othman Ahmed <othmanahmed258@gmail.com>
2026-04-07 21:05:10 +0200

    create: responsivness for description section in home page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import axiosInstance from "../../api/config";
 import { useState, useEffect } from "react";
 import { ClipLoader } from "react-spinners";
 import { useNavigate } from "react-router-dom";
+import { IoMenuOutline } from "react-icons/io5";
 
 const Navbar = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -64,66 +65,76 @@ const Navbar = () => {
   return (
     <nav className="container-fluid navbar">
       <div className="container">
-        <div className="navbar-col-1">
-          <Link className="navbar-brand" to="/">
-            <img src="/navbar-logo.png" alt="Logo" />
-          </Link>
-          <div className="language">
-            <button>
-              <img src="/language.svg" alt="Language icon" /> Global
-            </button>
-          </div>
-        </div>
-        <div className="navbar-col-2">
-          <Link className="search-btn" to="/projects">
-            <img src="/search.svg" alt="search icon" /> Search
-          </Link>
-          <Link to="/">Home</Link>
-          <Link to="/projects">Campaigns</Link>
-          <Link to="/about">About</Link>
-          <div>
-            <Link to="/create/">
-              <button className="create-btn">
-                <img src="/edit 1.svg" alt="edit icon" /> Start a campaign
-              </button>
-            </Link>
-          </div>
-          <div className="sign-btns d-flex align-items-center gap-2">
-            {isAuthenticated && user ? (
-              <>
-                <button className="logout-btn" onClick={handleLogout}>
-                  {isClicked ? <ClipLoader color="white" size={20} /> : "Sign out"}
+        {window.screen.width >= 1200 ? (
+          <>
+            <div className="navbar-col-1">
+              <Link className="navbar-brand" to="/">
+                <img src="/navbar-logo.png" alt="Logo" />
+              </Link>
+              <div className="language">
+                <button>
+                  <img src="/language.svg" alt="Language icon" /> Global
                 </button>
-                <img
-                  src={
-                    user?.profile_picture
-                      ? `https://ahmedelsabbagh.pythonanywhere.com${user.profile_picture}`
-                      : "/profile-blank.png"
-                  }
-                  alt="Profile"
-                  className="rounded-circle"
-                  style={{
-                    width: 40,
-                    height: 40,
-                    objectFit: "cover",
-                    border: "1px solid #ccc",
-                    cursor: "pointer",
-                  }}
-                  onClick={() => navigate(`/user/${user.id}`)}
-                />
-              </>
-            ) : (
-              <>
-                <Link className="signup-btn" to="/register">
-                  Sign up
+              </div>
+            </div>
+            <div className="navbar-col-2">
+              <Link className="search-btn" to="/projects">
+                <img src="/search.svg" alt="search icon" /> Search
+              </Link>
+              <Link to="/">Home</Link>
+              <Link to="/projects">Campaigns</Link>
+              <Link to="/about">About</Link>
+              <div>
+                <Link to="/create/">
+                  <button className="create-btn">
+                    <img src="/edit 1.svg" alt="edit icon" /> Start a campaign
+                  </button>
                 </Link>
-                <Link className="signin-btn" to="/login">
-                  Sign in
-                </Link>
-              </>
-            )}
-          </div>
-        </div>
+              </div>
+              <div className="sign-btns d-flex align-items-center gap-2">
+                {isAuthenticated && user ? (
+                  <>
+                    <button className="logout-btn" onClick={handleLogout}>
+                      {isClicked ? (
+                        <ClipLoader color="white" size={20} />
+                      ) : (
+                        "Sign out"
+                      )}
+                    </button>
+                    <img
+                      src={
+                        user?.profile_picture
+                          ? `https://ahmedelsabbagh.pythonanywhere.com${user.profile_picture}`
+                          : "/profile-blank.png"
+                      }
+                      alt="Profile"
+                      className="rounded-circle"
+                      style={{
+                        width: 40,
+                        height: 40,
+                        objectFit: "cover",
+                        border: "1px solid #ccc",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => navigate(`/user/${user.id}`)}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Link className="signup-btn" to="/register">
+                      Sign up
+                    </Link>
+                    <Link className="signin-btn" to="/login">
+                      Sign in
+                    </Link>
+                  </>
+                )}
+              </div>
+            </div>
+          </>
+        ) : (
+          <IoMenuOutline />
+        )}
       </div>
     </nav>
   );
